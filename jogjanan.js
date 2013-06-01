7b74c10b0cb5d90712ad9a8a2b9f8636f21218af
Angga Christana <anggachristana@gmail.com>
2013-06-01 20:05:14 +0700

    commit 1
    jogjanan v 1

---
+++ jogjanan.js
@@ -0,0 +1,76 @@
+//script navigasi jogjanan
+
+var jogjanan = new function(){
+	
+	this.programmer = "Angga Christana";
+	this.getInfo = function () {
+    	return this.programmer 
+		};
+
+	this.init = function () {
+		
+		document.addEventListener("deviceready",jogjanan.onDeviceReady,false);
+		
+		$( "#mainmenu" ).bind( "click", function( event, data ){
+         //class utk button utama tidak hilang
+         $( "#mainmenu" ).addClass('ui-btn-active');
+         
+      });
+		
+		$("#halamandepan").bind("pageshow", function(){
+			berandaxml.init("http://www.jogjanan.com/service/spotrandom.php");
+			});
+		
+		         //class utk button utama tidak hilang
+         			 
+		$("#search").on("pageshow", function(){
+			var param = jogjanan.searchpage('kata_kunci');
+			document.getElementById("searchparam").innerHTML="hasil pencarian untuk " +param+ ":";
+			var url ='http://jogjanan.com/service/spot_search.php/'+param;
+			searchpage.init(url);
+			 });
+			 
+		$("#event").bind("pagebeforeload",function(){
+			
+			eventxml.init("http://jogjanan.com/service/spotevent.php");
+			
+			});
+			 
+		         
+      
+	}
+	
+	this.onDeviceReady = function (){
+ 		
+		navigator.geolocation.getCurrentPosition(geoloc.onSuccess, geoloc.onError);
+   
+		}
+		
+	this.searchpage = function (name){
+		
+		name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+    	var regexS = "[\\?&]"+name+"=([^&#]*)";
+    	var regex = new RegExp( regexS );
+    	var results = regex.exec (window.location.href);
+    	if (results == null)
+        	return "";
+	        else
+    	    return results[1];
+			}
+			
+	}
+	
+var geoloc = new function(){
+		
+	this.onSuccess = function(position){
+		var element = document.getElementById('kaki');
+        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                            'Longitude: '          + position.coords.longitude             + '<br />' ;
+		}
+		
+	this.onError = function(){
+		alert('code: '    + error.code    + '\n' +
+                'message: ' + error.message + '\n');
+		}
+		
+	}
